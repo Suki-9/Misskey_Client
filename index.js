4618000dd0693bae2c43dd77639d1c75177592b6
suki-9 <suki.music2004@gmail.com>
2023-06-27 19:26:25 +0900

    AuthURLを生成できるようになったよ

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function GenUUID(){
-    document.getElementById("UUID").innerHTML =`UUID=${'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a) {
         let r = (new Date().getTime() + Math.random() * 16)%16 | 0, v = a == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
-    })}`
+    })
 }
 
-function GenOAuthURL() {
-    
+function GenMiAuthURL() {
+    const instanceURL = document.getElementById('instanceURL').value;
+    const UserID = document.getElementById('UserID').value;
+    const UUID = GenUUID().replace(/-/g, '');
+    const MiAauthURL = `https://${instanceURL}/miauth/${UUID}`
+    document.getElementById('MiAuthURL').innerHTML = 'MiAuthURL';
+    document.getElementById('MiAuthURL').href = MiAauthURL;
+
 }
