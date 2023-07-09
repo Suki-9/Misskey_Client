5525322dc453cfd74ea07b4752a0edce9dfc5d24
yu256 <122450013+yu256@users.noreply.github.com>
2023-07-10 01:02:52 +0900

    refactor（絵文字関連）

--- frontend/src/components/js/emoji.js
+++ frontend/src/components/js/emoji.js
@@ -1,50 +1,52 @@
 export default {
-  async search(query, host) {
-    const res = await fetch("http://192.168.11.2:3000/req/emojis/search", {
-      mode: "cors",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-        host: host,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      });
+	async search(query, host) {
+		const res = await fetch("http://192.168.11.2:3000/req/emojis/search", {
+			mode: "cors",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				query: query,
+				host: host,
+			}),
+		});
 
-    if (res.msg !== "success!") {
-      await this.get(host);
-      await this.search(query, host);
-    }
-    return res;
-  },
-  async get(host) {
-    const server = "192.168.11.2:3000";
-    await fetch(`http://${server}/req/emojis/get`, {
-      mode: "cors",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        host: host,
-      }),
-    });
-  },
-  async parse(user_name, mainhost, attr) {
-    const emoji = user_name.match(/:.*?:/g);
-    if (emoji !== null) {
-      const emoji_url = await this.search(emoji.join(""), mainhost);
-      for (let i = 0; i < emoji.length; i++)
-        user_name = user_name.replace(
-          emoji[i],
-          `<img class='${attr}_emoji' src='${emoji_url[emoji[i]]}'>`
-        );
-    }
-    return user_name;
-  },
+		const data = await res.json();
+
+		if (!res.ok) {
+			await this.get(host);
+			return this.search(query, host);
+		}
+
+		return data;
+	},
+
+	async get(host) {
+		const server = "192.168.11.2:3000";
+		await fetch(`http://${server}/req/emojis/get`, {
+			mode: "cors",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				host: host,
+			}),
+		});
+	},
+
+	async parse(user_name, mainhost, attr) {
+		const regex = /:.*:/g;
+		const matches = user_name.match(regex);
+
+		if (matches) {
+			const emoji_urls = await this.search(matches, mainhost);
+			user_name = user_name.replace(regex, (match) =>
+				`<img class='${attr}_emoji' src='${emoji_urls[match]}'>`
+			);
+		}
+
+		return user_name;
+	},
 };
