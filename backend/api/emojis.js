2b0dc666dca0ae6e9a494d545fd02cbead52399f
ゆー <122450013+yu256@users.noreply.github.com>
2023-07-10 02:00:06 +0900

    refactor（絵文字関連） (#1)

--- backend/api/emojis.js
+++ backend/api/emojis.js
@@ -1,66 +1,69 @@
 const fs = require('fs');
 
-const get = async(host) => {
-    const emo = await fetch(`https://${host}/api/emojis`)
-    .then((response) => response.json())
-    .then((data) => {return data.emojis});
+const getEmojis = async (host) => {
+	const emojis = await fetch(`https://${host}/api/emojis`)
+		.then((response) => response.json())
+		.then((data) => { return data.emojis });
 
-    fs.writeFile(`./emoji_index/${host}_emojis.json`, JSON.stringify(emo), (err) => {
-        if (err) {
-            return;
-        }
-    });
+	fs.writeFile(`./emoji_index/${host}_emojis.json`, JSON.stringify(emojis), (err) => {
+		if (err) {
+			return;
+		}
+	});
 
-    let categorys = [];
-    let emoji_index = [];
-    for (let i = 0;i < emo.length;i++) {
-        let category = emo[i].category;
-        const search = categorys.indexOf(category);
-        if (search == -1) {
-            categorys.push(category);
-            emoji_index.push([emo[i]])
-        } else {
-            emoji_index[search].push(emo[i])
-        }
-    }
+	const categories = [];
+	const emoji_index = [];
 
-    fs.writeFile(`./emoji_index/${host}_index_key.json`, JSON.stringify(categorys), (err) => {
-        if (err) {
-            return;
-        }
-    });
+	emojis.forEach(emoji => {
+		const category = emoji.category;
+		const search = categories.indexOf(category);
 
-    fs.writeFile(`./emoji_index/${host}_index.json`, JSON.stringify(emoji_index), (err) => {
-        if (err) {
-            return;
-        }
-    });
-    return;
+		if (search === -1) {
+			categories.push(category);
+			emoji_index.push([emoji]);
+		} else {
+			emoji_index[search].push(emoji);
+		}
+	});
+
+	fs.writeFile(`./emoji_index/${host}_index_key.json`, JSON.stringify(categories), (err) => {
+		if (err) {
+			return;
+		}
+	});
+
+	fs.writeFile(`./emoji_index/${host}_index.json`, JSON.stringify(emoji_index), (err) => {
+		if (err) {
+			return;
+		}
+	});
+	return;
 }
 
-const search = async(query,host) => {
-    const querys = query.match(/\:.*?\:/g);
-    const results = {}
+const searchEmoji = async (queries, host) => {
+	const results = {};
 
-    try{
-        const index = await JSON.parse(fs.readFileSync(`./emoji_index/${host}_emojis.json`, 'utf8'));
-        for (let i = 0;i < index.length;i++) {
-            for (let j = 0;j < querys.length;j++) {
-                if (index[i].name == querys[j].replace(/:/g,"")) {
-                    const key = querys[j];
-                    results[key] = index[i].url;
-                }
-            }
-        }
-    } catch(e) {
-        await get(host);
-        results.msg = "index error!";
-        return results;
-    }
+	try {
+		const index = JSON.parse(fs.readFileSync(`./emoji_index/${host}_emojis.json`, 'utf8'));
+		index.forEach((item) => {
+			queries.forEach((query) => {
+				if (item.name === query.replace(":", "")) {
+					const key = query;
+					results[key] = item.url;
+				}
+			});
+		});
+		results.statusCode = 200;
+	} catch (e) {
+		await getEmojis(host);
+		results.statusCode = 500;
+		// results.error = "index error!";
+	}
 
-    results.msg = "success!";
-    return results;
-}
+	return results;
+};
 
-exports.get = get
-exports.search = search
+module.exports = {
+	getEmojis,
+	searchEmoji
+};
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,30 @@
-const emojis = require('./api/emojis.js')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { getEmojis, searchEmoji } = require('./api/emojis.js');
 
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-const cors = require('cors')
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/test', function(req, res) {
-    res.send({
-        msg: "OK!"
-    })
-})
+app.get('/test', function (req, res) {
+	res.send({
+		msg: "OK!"
+	});
+});
 
-app.post('/req/emojis/get', function(req, res) {
-    emojis.get(req.body.host)
-    res.send({
-        msg: "OK!"
-    })
-})
-app.post('/req/emojis/search', async function(req, res) {
-    const result = await emojis.search(req.body.query,req.body.host);
-    res.send(result)
-})
-app.listen(process.env.PORT || 3000)
+app.post('/req/emojis/get', function (req, res) {
+	getEmojis(req.body.host);
+	res.send({
+		msg: "OK!"
+	});
+});
+
+app.post('/req/emojis/search', async function (req, res) {
+	const result = await searchEmoji(req.body.query, req.body.host);
+	res.send(result);
+});
+
+app.listen(process.env.PORT || 3000);
