0a309a3618a65955c12d27562bbc597f8103ff35
suki-9 <suki.music2004@gmail.com>
2023-07-24 16:14:31 +0900

    作り直しのためにfrontendを初期化

--- frontend/src/components/js/UUID.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-    Gen(){
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a) {
-            let r = (new Date().getTime() + Math.random() * 16)%16 | 0, v = a == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        })
-    }
-}
