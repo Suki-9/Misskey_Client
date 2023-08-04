006486f637861b24822b490bee0f3910981b2d03
suki-9 <suki.music2004@gmail.com>
2023-08-05 08:44:57 +0900

    引数に初期値を設定

--- frontend/src/scripts/API/note.ts
+++ frontend/src/scripts/API/note.ts
@@ -4,21 +4,20 @@ import { ModifiedNote, Note } from "../types";
 
 export const getNote = async (
   host: string,
-  channel?: string,
+  channel: string = "home",
+  maxIndexSize: number = 10,
   token?: string,
-  maxIndexSize?: number,
 ): Promise<ModifiedNote[]> => {
   const res: Note[] = await fetch(
-    `https://${host}/api/notes/${channel ?? "home"}-timeline`,
+    `https://${host}/api/notes/${channel}-timeline`,
     {
       method: "POST",
-
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         i: token ?? readCookie(`${host}_token`),
-        limit: maxIndexSize ?? 10,
+        limit: maxIndexSize,
       }),
     },
   )
