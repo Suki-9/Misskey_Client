a5e8722878a46c0f694588e6e1b150aba0a7312d
suki-9 <suki.music2004@gmail.com>
2023-08-05 11:13:59 +0900

    投稿ボタンを生やした

---
+++ frontend/src/components/global/Post.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+
+const CreatePost = () => { 
+
+}
+</script>
+
+
+<template>
+    <i
+        class="icon-pencil" 
+        :class="$style.postButton"
+        @click="CreatePost"></i>
+</template>
+
+
+<style module lang="postcss">
+.postButton {
+    position: absolute;
+    bottom: 10%;
+    right: 5%;
+
+    border-radius: 7px;
+
+    padding: 3%;
+
+    font-size: 1.5em;
+    line-height: 0;
+    color: var(--primary-bg-color);
+
+    background-color: var(--accent-color);
+}
+.postButton::before {
+    margin: 0;
+}
+</style>
