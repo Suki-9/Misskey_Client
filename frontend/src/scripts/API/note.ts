8d1998de232994866a2f1e11791bb11fbe7a9339
suki-9 <suki.music2004@gmail.com>
2023-08-05 14:36:45 +0900

    noteGen関数を移動

--- frontend/src/scripts/API/note.ts
+++ frontend/src/scripts/API/note.ts
@@ -1,6 +1,7 @@
 import { readCookie } from "../Cookie";
-import { noteGen } from "../note";
-import { ModifiedNote, Note } from "../types";
+import { ModifiedNote, Note, Reaction, User } from "../types";
+import { parseEmoji, searchEmoji } from "../emoji";
+
 
 export const getNote = async (
   host: string,
@@ -25,4 +26,47 @@ export const getNote = async (
     .then(data => data);
 
   return res.map(note => noteGen(note));
+};
+
+export const noteGen = (noteData: Note): ModifiedNote => {
+  const note: Note = noteData.renote ?? noteData;
+
+  // nameは初期設定だと空の場合があるので空であればidを使う
+  note.user.name ??= note.user.username;
+  noteData.user.name ??= noteData.user.username;
+
+  const reactions: Reaction[] = Object.entries(note.reactions).map(
+    ([reaction, count]) => {
+      return {
+        name: reaction,
+        count,
+        link: searchEmoji(reaction.slice(1, reaction.indexOf("@")))[0],
+      };
+    }
+  );
+
+  let renoter: User | undefined;
+
+  if (noteData.renote) {
+    renoter = {
+      name: parseEmoji(noteData.user.name),
+      username: noteData.user.username,
+      avatarUrl: noteData.user.avatarUrl,
+    };
+  }
+
+  return {
+    id: note.id,
+    createdAt: note.createdAt,
+    text: note.text && parseEmoji(note.text),
+    cw: note.cw,
+    user: {
+      name: parseEmoji(note.user.name),
+      username: note.user.username,
+      avatarUrl: note.user.avatarUrl,
+    },
+    files: note.files,
+    reactions,
+    renoter,
+  };
 };
