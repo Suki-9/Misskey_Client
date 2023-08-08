ef57b6a94d4379d36f602a751dcab19633c89980
suki-9 <suki.music2004@gmail.com>
2023-08-08 11:38:00 +0900

    必要なときのみ通信するように変更

--- src/scripts/API/userdata.ts
+++ src/scripts/API/userdata.ts
@@ -5,21 +5,23 @@ export const getUserData = async (
 	host: string,
 	token?: string
 ): Promise<string> => {
-  const res = await fetch(
-    `https://${host}/api/i`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        i: token ?? readCookie(`${host}_token`).unwrap(),
-      }),
-    }
-  )
-    .then(response => response.json())
-		.then(data => data);
+  if (!localStorage.getItem(`${host}_userData`)) {
+    const res = await fetch(
+      `https://${host}/api/i`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          i: token ?? readCookie(`${host}_token`).unwrap(),
+        }),
+      }
+    )
+      .then(response => response.json())
+      .then(data => data);
+    localStorage.setItem(`${host}_userData`, JSON.stringify(res));
+  }
 
-  localStorage.setItem(`${host}_userData`, JSON.stringify(res));
-  return JSON.stringify(res)
+  return JSON.stringify(localStorage.getItem(`${host}_userData`))
 };
