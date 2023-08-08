ee2c8f6d1663f378bb8e0669a431bf9490a4ec7a
suki-9 <suki.music2004@gmail.com>
2023-08-08 09:35:17 +0900

    治ってなかった

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 
 import "./assets/css/main.css";
+import "../src/scripts/global/result";
 
 import App from "./App.vue";
 import router from "./router";
+
 
 createApp(App).use(router).mount("#app");
