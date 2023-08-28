3414cb931cfb6eb354099f80de5fcf66bbc04515
suki-9 <suki.music2004@gmail.com>
2023-08-29 00:23:11 +0900

    今はいらないのでコメントアウト

--- src/assets/css/device/styleSwitch.js
+++ src/assets/css/device/styleSwitch.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) =>  {
-    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-      document.getElementById("PageStyleSheet").href = "E:/src/assets/css/device/mobile.css";
-    } else {
-      document.getElementById("PageStyleSheet").href = "E:/src/assets/css/device/PC.css";
-    }
-  });
+  //if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+  //  document.getElementById("PageStyleSheet").href = "E:/src/assets/css/device/mobile.css";
+  //} else {
+  //  document.getElementById("PageStyleSheet").href = "E:/src/assets/css/device/PC.css";
+  //}
+});
