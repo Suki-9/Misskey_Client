c7e927d2256b4c3a60b85356d0bdd7be560a2173
suki9 <suki.music2004@gmail.com>
2023-08-30 11:12:06 +0900

    型を修正

--- src/scripts/emoji.ts
+++ src/scripts/emoji.ts
@@ -7,7 +7,7 @@ import { Emoji } from "./types";
 
 const fetchEmojiIndex = async (
   host: string,
-): Promise<Emoji[]> => {
+): Promise<string> => {
   return await fetch(`https://${host}/api/emojis`)
     .then(response => response.json())
     .then(data => data.emojis);
@@ -17,19 +17,17 @@ const createEmojiIndex = async (
   type: string[],
   host: string,
 ) => {
-  const emojis = await fetchEmojiIndex(host);
+  let emojis = await fetchEmojiIndex(host);
   localStorage.setItem(`${host}_emojis`, JSON.stringify(emojis));
+
 
   //TODO ここ全て毎回評価が発生するので賢く死体
   if (type.indexOf("key") !== -1) {
-    localStorage.setItem(`${host}_emojis_key`, JSON.stringify(
-      emojis.map(emoji => emoji.name)
-    ));
+    localStorage.setItem(`${host}_emojis_key`, JSON.stringify(emojis.map(emoji => emoji.name)));
   }
 
   if (type.indexOf("category") !== -1) {
     let categorys: Record<string, string[]> = {}
-  
     emojis.forEach(emoji => {
       if (!categorys.hasOwnProperty(emoji.category)) categorys[emoji.category] = []
       categorys[emoji.category].push(emoji.name)
@@ -65,11 +63,11 @@ export const readEmojiIndex = (
   type?: string,
   host = readCookie("loginHost").unwrap()
 ) => {
-  let localEmojis = localStorage.getItem(`${host}_emojis${type ? "_" : ""}${type ?? ""}`);
+  let localEmojis = localStorage.getItem(`${host}_emojis${type ? `_${type}` : ""}`);
 
   if (!localEmojis) {
     createEmojiIndex([type ?? ""], host);
-    localEmojis = localStorage.getItem(`${host}_emojis${type ? "_" : ""}${type ?? ""}`);
+    localEmojis = localStorage.getItem(`${host}_emojis${type ? `_${type}` : ""}`);
   }
   return JSON.parse(localEmojis ?? "{}");
 }
