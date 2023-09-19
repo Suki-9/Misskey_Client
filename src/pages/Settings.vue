c038f4a63bd3ed084e2fbdc632e78f57de1bdf20
suki-9 <suki.music2004@gmail.com>
2023-09-20 00:55:18 +0900

    Add : Theme create tool

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+// Vue Component ---------------------------------------///
+import HeadBar from "../components/Settings/Head.vue"
+
+const headBarData: Record<string, {
+  text: string,
+  icon: string,
+}> = {
+  "/settings/themecreate": {
+    text: "テーマを作成",
+    icon: "icon-cog-outline",
+  },
+}
+</script>
+
 <template>
+  <HeadBar 
+    :text="headBarData[$route.path].text"
+    :icon="headBarData[$route.path].icon"
+  />
   <router-view />
 </template>
--- src/pages/Settings/ThemeCreate.vue
+++ src/pages/Settings/ThemeCreate.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+// TS Module -------------------------------------------///
+import { ref } from 'vue';
+
+const styleItems = ref<Record<string, string>>({
+  "--primary-bg-color": "#FFFFFF",
+  "--primary-text-color": "#FFFFFF",
+  "--secondary-text-color:": "#FFFFFF",
+  "--primary-border-color": "#FFFFFF",
+  "--shadow-color": "#FFFFFF",
+  "--accent-color": "#FFFFFF",
+})
+</script>
+
+
 <template>
-  <p>a</p>
+  <div v-for="item in Object.keys(styleItems)">
+    <p>{{ item }} : {{ styleItems[item] }}</p>
+    <input type="color" v-model="styleItems[item]">
+  </div>
 </template>
+
+
+<style module lang="scss">
+  p {
+    font-size: 90%;
+  }
+</style>
