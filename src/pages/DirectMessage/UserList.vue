93361184914e81e1bfb5fae6f9798463f0f5b8b1
suki-9 <suki.music2004@gmail.com>
2023-09-19 22:38:31 +0900

    Delete : Unused Module

--- src/pages/DirectMessage/UserList.vue
+++ src/pages/DirectMessage/UserList.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-//TS Module
-import { getNotifications } from "../../scripts/API/Notification";
-import { readCookie } from "../../scripts/cookie";
+
 </script>
 
 <template>
