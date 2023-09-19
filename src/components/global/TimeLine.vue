14dbacb54bd8e402a4bb2a2ce453d2e74cc60008
suki-9 <suki.music2004@gmail.com>
2023-09-20 00:58:26 +0900

    Change :  Style in Note in TimeLine

--- src/components/global/TimeLine.vue
+++ src/components/global/TimeLine.vue
@@ -106,7 +106,7 @@ if (props.hostName) {
   margin: 0 var(--primary-margin-w);
   padding-bottom: var(--bottom-bar-height);
   .note {
-    margin: var(--primary-margin-h) 0;
+    margin-bottom: var(--primary-margin-w);
   }
 }
 </style>
