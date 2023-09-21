4ef01c049970ff0036efb08d261fcc0c2663409e
suki-9 <suki.music2004@gmail.com>
2023-09-21 15:41:40 +0900

    Add : Head Bar from NoteView

--- src/components/global/HeadBar.vue
+++ src/components/global/HeadBar.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 defineProps<{
-  text: string;
-  icon: string;
+  text?: string;
+  icon?: string;
 }>();
 </script>
 
 <template>
   <div :class="$style.root">
     <i :class="icon"></i>
+    <img :class="$style.emoji" :src="icon" v-show="icon"/>
     <p>{{ text }}</p>
   </div>
 </template>
 
 <style module lang="scss">
+@import "../../styles/globalComponent.css";
 .root {
   display: flex;
   align-items: center;
@@ -21,5 +23,9 @@ defineProps<{
   height: var(--head-bar-height);
 
   border-bottom: solid 1px var(--primary-border-color);
+
+  p {
+    margin-left: 1%;
+  }
 }
 </style>
--- src/pages/NoteView.vue
+++ src/pages/NoteView.vue
@@ -6,6 +6,8 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 
 // Vue Component ---------------------------------------///
+import Note from "../components/global/Note.vue";
+import HeadBar from "../components/global/HeadBar.vue";
 
 const note = await fetchMisskeyAPI<"notes/show">("notes/show", {
   noteId: route.params["id"] as string,
@@ -13,7 +15,17 @@ const note = await fetchMisskeyAPI<"notes/show">("notes/show", {
 </script>
 
 <template>
-  <p>{{ note }}</p>
+  <HeadBar
+    :text="`${note.createdAt.split('T')[0]}(${note.createdAt.split('T')[1].split('.')[0]})`"
+    :icon="note.user.avatarUrl"
+  />
+  <Note 
+  :class="$style.note"
+  :note="note"/>
 </template>
 
-<style module lang="scss"></style>
+<style module lang="scss">
+.note {
+  margin: 2%;
+}
+</style>
