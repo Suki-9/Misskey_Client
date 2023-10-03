f3bb3721487a51953f696fefb0ef60c247fad245
suki9 <suki.music2004@gmail.com>
2023-10-03 13:42:43 +0900

    refactor : "Cookieの検索を高速化"

--- src/scripts/cookie.ts
+++ src/scripts/cookie.ts
@@ -1,12 +1,10 @@
-export const getCookie = (): [string[][], string[]] => {
-  const cookies = document.cookie.split("; ").map(cookie => cookie.split("="));
-  const cookieNames = cookies.map(cookie => cookie[0]);
-  return [cookies, cookieNames];
-};
+export const cookieIndex: {[key: string]: string} = document.cookie.split("; ").reduce((accumulator, value) => {
+  const kv = value.split("=");
+  return { ...accumulator, [kv[0]]: kv[1] };
+}, {})
 
 export const readCookie = (key: string): Result<string, Error> => {
-  const [cookies, cookieNames] = getCookie();
-  const i = cookieNames.indexOf(key);
-  if (i === -1) return new Err(new Error(`failed to read cookie "${key}".`));
-  return new Ok(cookies[i][1]);
-};
+  return (cookieIndex[key]) 
+    ? new Ok(cookieIndex[key])
+    : new Err(new Error(`failed to read cookie "${key}".`));
+}
