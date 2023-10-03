02e261e53a98f3e9177deb2f6ea895c5a599cdf4
suki-9 <suki.music2004@gmail.com>
2023-10-03 23:46:32 +0900

    refactor : "emojiの検索を最適化"

--- src/scripts/emoji.ts
+++ src/scripts/emoji.ts
@@ -4,54 +4,54 @@ import { readCookie } from "./cookie";
 //Type
 import { Emoji } from "./types";
 
+type emojiIndex = Record<string, {
+  category: string;
+  aliases: string[];
+  name: string;
+  url: string;
+}>;
+
 const fetchEmojiIndex = async (host: string): Promise<Emoji[]> => {
   return await fetch(`https://${host}/api/emojis`)
     .then(response => response.json())
     .then(data => data.emojis);
 };
 
-const createEmojiIndex = async (type: string[], host: string) => {
-  let emojis = await fetchEmojiIndex(host);
-  localStorage.setItem(`${host}_emojis`, JSON.stringify(emojis));
+export const createEmojiIndex = async (host: string) => {
+  let emojis: emojiIndex = (await fetchEmojiIndex(host)).reduce((accumulator, value) => ({ ...accumulator, [value.name]: value }), {});
 
-  //TODO ここ全て毎回評価が発生するので賢く死体
-  if (type.indexOf("key") !== -1) {
-    localStorage.setItem(`${host}_emojis_key`, JSON.stringify(emojis.map(emoji => emoji.name)));
-  }
+  let categorys: Record<string, string[]> = {};
 
-  if (type.indexOf("category") !== -1) {
-    let categorys: Record<string, string[]> = {};
-    emojis.forEach(emoji => {
-      if (!categorys.hasOwnProperty(emoji.category)) categorys[emoji.category] = [];
-      categorys[emoji.category].push(emoji.name);
-    });
-    localStorage.setItem(`${host}_emojis_category`, JSON.stringify(categorys));
-  }
-};
-
-export const searchEmoji = (name: string, host = readCookie("loginHost").unwrap()): Result<string, Error> => {
-  const index = readEmojiIndex("key", host);
-  const emojis: Emoji[] = readEmojiIndex(undefined, host);
-  if (index.indexOf(name.slice(1, name.indexOf("@"))) === -1) return new Err(new Error(name));
-
-  return new Ok(emojis[index.indexOf(name.slice(1, name.indexOf("@")))].url);
-};
-
-export const parseEmoji = (text: string) => {
-  const matches = text.match(/:.*?:/g);
-  matches?.forEach(emoji => {
-    const url = searchEmoji(emoji);
-    if (url.isOk()) text = text.replace(emoji, `<img class="emoji" src="${url.value}">`);
+  Object.keys(emojis).forEach(key => {
+    if (!categorys.hasOwnProperty(emojis[key].category)) categorys[emojis[key].category] = [];
+    categorys[emojis[key].category].push(key);
   });
-  return text;
+
+  localStorage.setItem(`${host}_emojis`, JSON.stringify(emojis));
+  localStorage.setItem(`${host}_emojis_category`, JSON.stringify(categorys));
 };
 
 export const readEmojiIndex = (type?: string, host = readCookie("loginHost").unwrap()) => {
   let localEmojis = localStorage.getItem(`${host}_emojis${type ? `_${type}` : ""}`);
 
   if (!localEmojis) {
-    createEmojiIndex([type ?? ""], host);
+    createEmojiIndex(host);
     localEmojis = localStorage.getItem(`${host}_emojis${type ? `_${type}` : ""}`);
   }
-  return JSON.parse(localEmojis ?? "{}");
+  return localEmojis && JSON.parse(localEmojis);
+};
+
+export const searchEmoji = (name: string, host = readCookie("loginHost").unwrap()): Result<string, Error> => {
+  const index: emojiIndex = readEmojiIndex(undefined, host);
+  return index[name.slice(1, name.indexOf("@"))]
+    ? new Ok(index[name.slice(1, name.indexOf("@"))].url)
+    : new Err(new Error(name));
+};
+
+export const parseEmoji = (text: string) => {
+  text.match(/:.*?:/g)?.forEach(emoji => {
+    const url = searchEmoji(emoji);
+    if (url.isOk()) text = text.replace(emoji, `<img class="emoji" src="${url.value}">`);
+  });
+  return text;
 };
