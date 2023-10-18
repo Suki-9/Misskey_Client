f7f3e3941563ed5ace99a6f431bb217d32d8869d
suki-9 <suki.music2004@gmail.com>
2023-10-18 21:50:32 +0900

    Result型ではなく失敗時はundefinedを返す

--- src/scripts/cookie.ts
+++ src/scripts/cookie.ts
@@ -3,6 +3,6 @@ export const cookieIndex: { [key: string]: string } = document.cookie.split("; "
   return { ...accumulator, [kv[0]]: kv[1] };
 }, {});
 
-export const readCookie = (key: string): Result => {
-  return cookieIndex[key] ? ok<string>(cookieIndex[key]) : err<string>(new Error(`failed to read cookie "${key}".`));
+export const readCookie = (key: string): string | undefined => {
+  return cookieIndex[key];
 };
