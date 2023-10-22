64cef232a520aa22225c79beb153c12527f4a1bd
suki-9 <suki.music2004@gmail.com>
2023-10-22 22:19:23 +0900

    fix "サイト全体からinjectできるように"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
-//vue component
+//vue component ----------------------------------------///
 import BottomBar from "./components/global/BottomBar.vue";
+
+// TS module -------------------------------------------///
+import { readCookie } from "./scripts/cookie";
+import { provide } from "vue";
+import { UserData } from "./scripts/Types/types";
+
+const
+  loginUser = readCookie("loginUser"),
+  loginUserData = localStorage.getItem(`${loginUser.value}_UserData`),
+  usersData = localStorage.getItem("usersData");
+
+if (loginUser.value) { 
+  if (loginUserData) provide<UserData>("loginUserData", JSON.parse(loginUserData))
+  if (usersData) provide<LoginUser>("loginUser", JSON.parse(usersData)[loginUser.value])
+}
 </script>
 
 <template>
