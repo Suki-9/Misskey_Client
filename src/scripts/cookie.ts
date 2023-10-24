6a7559f449469bdc526945c70f6c5581049c005b
suki-9 <suki.music2004@gmail.com>
2023-10-24 12:00:50 +0900

    create : "ラッパー関数を作成"

--- src/scripts/cookie.ts
+++ src/scripts/cookie.ts
@@ -6,3 +6,12 @@ export const cookieIndex: { [key: string]: string } = document.cookie.split("; "
 export const readCookie = (key: string): Result<string | undefined> => {
   return cookieIndex[key] ? { value: cookieIndex[key], isOk: true } : { value: undefined, isOk: false };
 };
+
+// TODO 何かしら返したほうがいいかも
+export const writeCookie = (key: string, value?: string): void => {
+  document.cookie = `${key}=${value}; expires=0; path=/`;
+}
+
+export const deleteCookie = (key: string): void => {
+  document.cookie = `${key}=; expires=0; path=/`;
+}
