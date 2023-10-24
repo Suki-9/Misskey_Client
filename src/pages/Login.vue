44f5c3a3352b9476da71978c1a56ca6c1050fe01
suki9 <suki.music2004@gmail.com>
2023-10-24 14:52:33 +0900

    fix : "成形時に破壊されちゃってた"

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -28,9 +28,7 @@ const Login = async () => {
     }
   } else if (useMiAuth.value && hostURL.value) {
     document.cookie = `loginHost=${hostURL.value}; path=/`;
-    window.location.href = `${
-      hostURL.value
-    }/miauth/${genUuid()}?name=Kurage&callback=${$HOST_URL}/callback&permission=read:account,write:account,read:blocks,write:blocks,read:drive,write:drive,read:favorites,write:favorites,read:following,write:following,read:messaging,write:messaging,read:mutes,write:mutes,write:notes,read:notifications,write:notifications,write:reactions,write:votes,read:pages,write:pages,write:page-likes,read:page-likes,write:gallery-likes,read:gallery-likes`;
+    window.location.href = `${hostURL.value}/miauth/${genUuid()}?name=Kurage&callback=${$HOST_URL}/callback&permission=read:account,write:account,read:blocks,write:blocks,read:drive,write:drive,read:favorites,write:favorites,read:following,write:following,read:messaging,write:messaging,read:mutes,write:mutes,write:notes,read:notifications,write:notifications,write:reactions,write:votes,read:pages,write:pages,write:page-likes,read:page-likes,write:gallery-likes,read:gallery-likes`;
   }
 };
 </script>
