b3376f253c38e44fa505de82cc009b03404a805e
suki-9 <suki.music2004@gmail.com>
2024-03-29 05:32:29 +0900

    Remake LoginPage

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,178 +1,88 @@
 <script setup lang="ts">
-import TextInput from "../components/Login/TextInput.vue";
+import KuTextInput from "@/components/global/KuTextInput.vue";
+import KuToggleSwitch from '@/components/global/KuToggleSwitch.vue';
+import KuButton from "@/components/global/KuButton.vue";
 
+import cookie from '@/scripts/cookie';
 import { $HOST_URL } from "../plugin/vite_env";
 import { genUuid } from "../scripts/UUID";
-import { useRouter } from "vue-router";
+import { IdexedDB } from "@/scripts/indexedDB";
+//import { useRouter } from "vue-router";
 import { ref } from "vue";
-import { addUsersData } from "../scripts/API/userdata";
-import cookie from "../scripts/cookie";
+import { api } from "misskey-js";
+import router from "@/plugin/router_index";
+//const router = useRouter();
 
-const router = useRouter();
-
-const avatarUrl = ref<string>();
 const hostURL = ref<string>();
 const token = ref<string>();
-const useToken = ref<boolean>(false);
 const useMiAuth = ref<boolean>(true);
 
 const Login = async () => {
-  if (useToken.value && token.value && hostURL.value && !useMiAuth.value) {
-    const loginUser = await addUsersData(hostURL.value, token.value);
-    if (loginUser) {
-      cookie.write("loginUser", loginUser);
-      router.push("/");
+  if (!useMiAuth.value && token.value && hostURL.value) {
+    if (hostURL.value.slice(-1)[0] == '/') hostURL.value = hostURL.value.slice(0, -1);
+    const mk = new api.APIClient({
+      origin: hostURL.value,
+      credential: token.value,
+    });
+    const userData = await mk.request('i', {});
+    const db = new IdexedDB();
+    const result = db.addUser({
+      host: hostURL.value,
+      token: token.value,
+      userId: userData.id,
+      userData: userData
+    });
+    if (await result.then(() => true).catch(() => false)) {
+      router.push('/');
     } else {
-      alert("ログインに失敗しました！");
-      router.push("/login");
+      //失敗時処理
     }
   } else if (useMiAuth.value && hostURL.value) {
-    cookie.write("loginHost", hostURL.value);
-    window.location.href = `${
-      hostURL.value
-    }/miauth/${genUuid()}?name=Kurage&callback=${$HOST_URL}/callback&permission=read:account,write:account,read:blocks,write:blocks,read:drive,write:drive,read:favorites,write:favorites,read:following,write:following,read:messaging,write:messaging,read:mutes,write:mutes,write:notes,read:notifications,write:notifications,write:reactions,write:votes,read:pages,write:pages,write:page-likes,read:page-likes,write:gallery-likes,read:gallery-likes`;
+    if (hostURL.value.slice(-1)[0] == '/') hostURL.value = hostURL.value.slice(0, -1);
+    cookie.write('loginHost', hostURL.value);
+    window.location.href = `${hostURL.value}/miauth/${genUuid()}?name=Kurage&callback=${$HOST_URL}/callback&permission=read:account,write:account,read:blocks,write:blocks,read:drive,write:drive,read:favorites,write:favorites,read:following,write:following,read:messaging,write:messaging,read:mutes,write:mutes,write:notes,read:notifications,write:notifications,write:reactions,write:votes,read:pages,write:pages,write:page-likes,read:page-likes,write:gallery-likes,read:gallery-likes`;
   }
 };
 </script>
 
 <template>
-  <div :class="$style.root">
-    <p :class="$style.head">Login!!{{ useMiAuth ? "(Use MiAuth)" : useToken ? "(Use Token)" : "" }}</p>
-    <div :class="$style.tile">
-      <p :class="$style.tileHead">ログイン<span></span></p>
-      <div :class="$style.tileContainer">
-        <img :class="$style.avatar" :src="avatarUrl" />
-        <a :class="$style.button" @click="(useToken = false), (useMiAuth = true)">Use MiAuth</a>
-        <a :class="$style.button" @click="(useToken = true), (useMiAuth = false)">Use Token</a>
-      </div>
-      <!--
-      <TextInput :placeholder="'UserName'" @receive="e => (userName = e)" v-show="!useToken && !useMiAuth" />
-      <TextInput :placeholder="'PassWord'" @receive="e => (password = e)" v-show="!useToken && !useMiAuth" />
-      -->
-      <TextInput :placeholder="'Your Token'" @receive="e => (token = e)" v-show="useToken && !useMiAuth" />
-      <TextInput :placeholder="'Host Name'" @receive="e => (hostURL = e)" />
-      <p :class="$style.attentionText" v-show="!useToken">パスワードを忘れた場合</p>
-      <div :class="$style.submitButtonBox">
-        <a :class="$style.submitButton" @click="Login">Login</a>
-      </div>
-    </div>
-    <!--
-    <div :class="$style.tile">
-      <p :class="$style.tileHead">他のサーバーにログインする<span></span></p>
-      <TextInput :placeholder="'Host Name'" @receive="e => (hostURL = e)" />
-    </div>
-    <div :class="$style.tile" v-if="false">
-      <p :class="$style.tileHead">新規登録<span></span></p>
-      <TextInput :placeholder="'UserName'" @receive="e => (userName = e)" />
-      <TextInput :placeholder="'Password'" @receive="e => (password = e)" />
-    </div>
-    -->
+  <div :class="$style.head">
+    <h4>Kurage</h4>
+    <p>misskey third party client.</p>
   </div>
+  <KuToggleSwitch :class="$style.inputs" label="Use MiAuth" v-model="useMiAuth" />
+  <KuTextInput :class="$style.inputs" name="Host origin" placeholder="https://exmple.tld" v-model:text="hostURL" />
+  <KuTextInput :class="$style.inputs" name="Your Token" placeholder="Some alphanumeric characters." v-model:text="token"
+    v-show="!useMiAuth" />
+  <KuButton :action="Login" text="login" :is-disabled="!Boolean(useMiAuth ? hostURL : hostURL && token)" />
 </template>
 
+<style>
+#app {
+  height: 100%;
+  margin: 0 5%;
+}
+</style>
+
 <style module lang="scss">
-.root {
+.inputs {
+  margin: 0 0 0.5em 0;
+}
+
+.head {
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
 
-  width: 100vw;
-  height: 100vh;
-  .head {
-    display: flex;
-    align-items: center;
+  margin: 2em 0;
 
-    height: 6.25vh;
-
-    padding: 0 5vw;
-
-    font-size: 110%;
-
-    border-bottom: solid 1px var(--primary-border-color);
+  h4 {
+    font-size: 140%;
   }
-  .tile {
-    position: relative;
 
-    margin: 1.25vh 5vw 0 5vw;
-
-    .tileHead {
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-
-      height: 2.5vh;
-
-      margin: 0 0 1.25vh 0;
-
-      font-size: 70%;
-
-      white-space: nowrap;
-      span {
-        padding: 1px 0 0 0;
-
-        width: 100%;
-
-        border-bottom: solid 1px var(--primary-border-color);
-      }
-    }
-    .tileContainer {
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-      justify-content: space-between;
-
-      width: 100%;
-
-      margin: 0 0 1.25vh 0;
-      .avatar {
-        height: 7.5vh;
-        width: 7.5vh;
-
-        font-size: 70%;
-
-        border-radius: 50%;
-        border: solid 1px var(--primary-border-color);
-      }
-      .button {
-        width: 30vw;
-
-        padding: 1vh 1vw;
-
-        font-size: 70%;
-        text-align: center;
-
-        border-radius: var(--default-border-radius);
-        border: solid 1px var(--primary-border-color);
-      }
-    }
-    .attentionText {
-      margin: 0 0 1.25vh 0;
-
-      padding: 0 2.5vw;
-
-      font-size: 50%;
-      text-align: right;
-      color: var(--secondary-text-color);
-    }
-    .submitButtonBox {
-      width: 100%;
-      height: 3.75vh;
-
-      margin: 0 0 1.25vh 0;
-
-      text-align: right;
-      .submitButton {
-        height: 3.75vh;
-        width: 25vw;
-
-        padding: 1vh 7.5vw;
-
-        font-size: 70%;
-        text-align: center;
-
-        border-radius: var(--default-border-radius);
-        border: solid 1px var(--primary-border-color);
-      }
-    }
+  p {
+    color: var(--placeholder-text-color);
   }
 }
 </style>
---
+++ src/pages/Login/Callback.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import cookie from "../../scripts/cookie";
+import { useRoute, useRouter } from "vue-router";
+import { IdexedDB } from "@/scripts/indexedDB";
+import { api } from 'misskey-js';
+
+const isArray = Array.isArray;
+const router = useRouter();
+const route = useRoute();
+
+const host = cookie.read('loginHost');
+const session = route.query["session"];
+if (host && !isArray(session)) { 
+  const token = await fetch(`http://192.168.11.4:3200/miauth/authorize?session=${session}&host=${host}`, {
+  })
+    .then((r) => r.json())
+    .then((res) => res.token)
+    .catch(() => undefined);
+  if (token) {
+    const mk = new api.APIClient({
+      origin: host,
+      credential: token,
+    });
+    const userData = await mk.request('i', {});
+    const db = new IdexedDB();
+    const result = db.addUser({
+      host: host,
+      token: token,
+      userId: userData.id,
+      userData: userData
+    });
+    if (await result.then(() => true).catch(() => false)) {
+      router.push('/');
+    } else {
+      //失敗時処理
+    }
+  }
+}
+</script>
+
+<template>
+  <p style="text-align: center">少し待ってね。</p>
+</template>
