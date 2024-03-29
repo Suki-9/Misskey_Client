616bd7784f178d30093ac2b2bd2289c852f7a91c
suki-9 <suki.music2004@gmail.com>
2024-03-29 20:12:51 +0900

    delete script and component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,62 +1,11 @@
 <script setup lang="ts">
-import TimeLine from "../components/Home/TimeLine.vue";
-import Post from "../components/global/PostNote.vue";
-import SideSwipeMenu from "../components/Home/SideSwipeMenu.vue";
-
-import cookie from "../scripts/cookie";
-import { useRouter } from "vue-router";
-import { ref } from "vue";
-import { useSwipeMenu } from "../scripts/SideSwipeMenu";
-
-// トークンの有無を確認 --------------------------------///
-const loginUser = cookie.read("loginUser");
-//TODO ここの any
-const usersData: Record<string, {
-  avatarURL: string,
-  host: string,
-  token: string,
-  userName: string,
-}> = JSON.parse(localStorage.getItem("usersData")!);
-
-if (loginUser) {
-  // Animation -----------------------------------------///
-  useSwipeMenu();
-} else {
-  useRouter().push("/login");
-}
-
-// 仮
-const selectTimeLine = ref<{
-  channel?: "Home" | "hybrid" | "local" | "global";
-  autoReConnection: boolean;
-  hostName: string;
-  token: string;
-}>({
-  channel: "local",
-  autoReConnection: true,
-  hostName: String(loginUser?.split("_")[0]),
-  token: loginUser && usersData[loginUser].token,
-});
+import TimeLine from '@/components/Home/TimeLine.vue';
 </script>
 
 <template>
-  <SideSwipeMenu />
-  <div :class="$style.hoverPage" id="hoverPage">
-    <TimeLine :selectTimeLine="selectTimeLine" />
-  </div>
-  <Post />
+  <TimeLine :channel="'homeTimeline'"/>
 </template>
 
 <style module lang="scss">
-.hoverPage {
-  position: fixed;
-  top: 0;
 
-  height: var(--display-height);
-  width: 100%;
-
-  overflow-y: scroll;
-
-  background-color: var(--primary-bg-color);
-}
 </style>
