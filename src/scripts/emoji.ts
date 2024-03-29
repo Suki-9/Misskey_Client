5501c329ac2319a61413e18d63688fa610afe818
suki-9 <suki.music2004@gmail.com>
2024-03-29 20:08:46 +0900

    Add Emojis script

--- src/scripts/emoji.ts
+++ src/scripts/emoji.ts
@@ -1,98 +1,61 @@
-import { VNode, h } from "vue";
+import { api } from "misskey-js";
+import { components } from 'misskey-js/autogen/types.js';
+import { IndexedDB } from "./indexedDB";
 
-//TODO 突貫工事なので後々整理
+type Mi_Emoji = components['schemas']['EmojiSimple'];
+type Mi_EmojiIndex = {
+  [key: string]: Mi_Emoji;
+};
+type Mi_EmojisCategory = Record<string, string[]>;
+
 export class Emoji {
   public index: Mi_EmojiIndex = {};
+  private categoryIndex: Mi_EmojisCategory = {};
+  private db = new IndexedDB();
 
   constructor(host: string) {
     this.init(host);
   }
 
-  private createEmojiIndex = async (host: string): Promise<void> => {
-    localStorage.setItem(`${host}_emojis`, JSON.stringify((
-      await fetch(`${host}/api/emojis`)
-        .then(response => response.json())
-        .then((data): Mi_Emoji[] => data.emojis)
-    )
-      .reduce((accumulator, v) => ({ ...accumulator, [v.name]: v }), {})));
-  };
+  private modifyIndex = <T extends "category" | undefined>(emojis: Mi_Emoji[], type?: T): OptionalArgBranch<T, Mi_EmojiIndex, Mi_EmojisCategory> =>
+    type
+      ? emojis.reduce<Mi_EmojisCategory>((a, e) => {
+        for (const c of e.category ?? []) if (!(c in a)) a[c] = [];
+        a[e.name].push(e.name);
+        return a;
+      }, {})
+      : emojis.reduce((a, e) => {
+        return { ...a, [e.name]: e };
+      }, {});
 
   private async init(host: string) {
-    this.index = JSON.parse((host && (localStorage.getItem(`${host}_emojis`) ?? (await this.createEmojiIndex(host), localStorage.getItem(`${host}_emojis`)))) ?? '{}')
+    const result = this.db.readEmojisIndex('host');
+    if (await result.then(() => true).catch(() => false)) {
+      this.index = this.modifyIndex(await result);
+      this.categoryIndex = this.modifyIndex(await result, 'category');
+    }
+    else this.createEmojiIndex(host, this.db)
   }
+
+  public createEmojiIndex = async (host: string, db: IndexedDB) => {
+    const mk = new api.APIClient({
+      origin: host,
+    });
+    const index = await mk.request('emojis', {});
+    const result = db.addEmojisIndex(host, index.emojis).then(() => true).catch(() => false);
+    if (await result.then(() => true).catch(() => false)) {
+      this.index = this.modifyIndex(index.emojis);
+      this.categoryIndex = this.modifyIndex(index.emojis, 'category');
+    };
+  };
 
   public read<T extends "category" | undefined>(
     type?: "category",
   ): OptionalArgBranch<T, Mi_EmojiIndex, Mi_EmojisCategory> {
     return <OptionalArgBranch<T, Mi_EmojiIndex, Mi_EmojisCategory>>(type
-      ? JSON.stringify(Object.values(this.index).reduce<{ [k: string]: string[] }>(
-        (accumulator, emoji) =>
-          Object.prototype.hasOwnProperty.call(accumulator, emoji.category)
-            ? (accumulator[emoji.category].push(emoji.name), accumulator)
-            : (accumulator[emoji.category] = [emoji.name], accumulator)
-        , {}))
+      ? this.categoryIndex
       : this.index);
   }
 
-  public search = (name: string): string | undefined =>
-    this.index[name.slice(1, name.indexOf("@"))]?.url;
-
-  public parse = <T extends boolean | undefined>(
-    text: string,
-    vnode?: T
-  ): OptionalArgBranch<T, string, (VNode | string)[]> => {
-    return <OptionalArgBranch<T, string, (VNode | string)[]>>text.split(/(:.*?:)/g).map(item => {
-      if (item.match(/:.*?:/g)) {
-        const url = this.search(item);
-        return url
-          ? vnode
-            ? h(
-              "img",
-              { class: "emoji", src: url, }
-            )
-            : `<img class="emoji" src="${url}">`
-          : item;
-      } else {
-        return item;
-      }
-    });
-  };
+  public search = (name: string): Mi_Emoji | undefined => this.index[name];
 }
-
-// 置換が完了次第削除
-export const searchEmoji = (name: string, host: string): string | undefined =>
-  new Emoji(host).index[name.slice(1, name.indexOf("@"))]?.url
-
-// 置換が完了次第削除
-export const readEmojiIndex = <T extends "category" | undefined>(
-  host: string,
-  type?: T
-): OptionalArgBranch<T, Mi_EmojiIndex, Mi_EmojisCategory> => {
-  return <OptionalArgBranch<T, Mi_EmojiIndex, Mi_EmojisCategory>>(type
-    ? new Emoji(host).index
-    : new Emoji(host).index);
-};
-
-//TODO 常に VNode で返すようにするべきかも
-// 置換が完了次第削除
-export const parseEmoji = <T extends boolean | undefined>(
-  text: string,
-  host: string,
-  vnode?: T
-): OptionalArgBranch<T, string, (VNode | string)[]> => {
-  return <OptionalArgBranch<T, string, (VNode | string)[]>>text.split(/(:.*?:)/g).map(item => {
-    if (item.match(/:.*?:/g)) {
-      const url = searchEmoji(item, host);
-      return url
-        ? vnode
-          ? h("img", {
-              class: "emoji",
-              src: url,
-            })
-          : `<img class="emoji" src="${url}">`
-        : item;
-    } else {
-      return item;
-    }
-  });
-};
--- src/scripts/indexedDB.ts
+++ src/scripts/indexedDB.ts
@@ -1,13 +1,18 @@
 import Dexie, { Table } from 'dexie';
-import { entities } from "misskey-js";
+import { components } from 'misskey-js/autogen/types.js';
 
-export class IdexedDB extends Dexie {
+export class IndexedDB extends Dexie {
   private misskeyLiginUser?: Table<LoginUser>;
+  private emojisIndex?: Table<{
+    host: string,
+    index: components['schemas']['EmojiSimple'][],
+  }>;
 
   constructor() {
     super('kurageDB');
     this.version(1).stores({
       misskeyLiginUser: 'userId, userName, token, avatarURL, host, userData',
+      emojisIndex: 'host, index',
     });
   }
 
@@ -21,9 +26,29 @@ export class IdexedDB extends Dexie {
     }
   }
 
+  public async emojisIndexUser() {
+    if ('emojisIndex' in this) {
+      this.emojisIndex?.clear();
+    }
+  }
+
   public async addUser(userData: LoginUser) {
     if ('misskeyLiginUser' in this) {
       this.misskeyLiginUser?.put(userData);
+    }
+  }
+  
+  public async addEmojisIndex(host: string, index: components['schemas']['EmojiSimple'][]): Promise<void> {
+    if ('misskeyLiginUser' in this) {
+      this.emojisIndex?.put({
+        host: host,
+        index: index,
+      }).catch(() => {
+        alert('updateIndex faild');
+        throw new Error("Fetch Err.");
+      })
+    } else {
+      throw new Error("DB Err.");
     }
   }
 
@@ -36,6 +61,14 @@ export class IdexedDB extends Dexie {
     } else {
       return [];
     }
+  }
+
+  public async readEmojisIndex(host: string): Promise<components['schemas']['EmojiSimple'][]> {
+    if ('emojisIndex' in this) {
+      const result = this.emojisIndex?.where({ host: host }).toArray();
+      if (result && await result.then(() => true).catch(() => false)) return (await result)[0].index;
+    }
+    throw new Error("なんかえらー");
   }
 
   public async deleteUSer(userId: string) {
