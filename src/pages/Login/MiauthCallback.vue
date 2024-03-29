e9ff288fb2de6487af4d5dab8a7fe8dba0207336
suki-9 <suki.music2004@gmail.com>
2024-03-29 20:09:30 +0900

    fix callback page

--- src/pages/Login/MiauthCallback.vue
+++ src/pages/Login/MiauthCallback.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import cookie from "../../scripts/cookie";
 import { useRoute, useRouter } from "vue-router";
-import { IdexedDB } from "@/scripts/indexedDB";
+import { IndexedDB } from "@/scripts/indexedDB";
 import { api } from 'misskey-js';
+
 
 const isArray = Array.isArray;
 const router = useRouter();
@@ -10,31 +11,33 @@ const route = useRoute();
 
 const host = cookie.read('loginHost');
 const session = route.query["session"];
-if (host && !isArray(session)) { 
-  const token = await fetch(`http://192.168.11.4:3200/miauth/authorize?session=${session}&host=${host}`, {
+if (host && !isArray(session)) {
+  const token = await fetch(`https://kurage-dev.kawaii-music.xyz/api/miauth/authorize?session=${session}&host=${host}`, {
   })
     .then((r) => r.json())
     .then((res) => res.token)
-    .catch(() => undefined);
+    .catch(() => router.push('/login'));
   if (token) {
     const mk = new api.APIClient({
       origin: host,
       credential: token,
     });
     const userData = await mk.request('i', {});
-    const db = new IdexedDB();
+    const db = new IndexedDB();
     const result = db.addUser({
       host: host,
       token: token,
       userId: userData.id,
-      userData: userData
+      userData: userData,
     });
     if (await result.then(() => true).catch(() => false)) {
       router.push('/');
     } else {
-      //失敗時処理
+      router.push('/login');
     }
   }
+} else {
+  router.push('/login');
 }
 </script>
 
